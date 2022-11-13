d1fe0fdcc99e5984c12b1522d840ff8c66588437
wuriyanto <wuriyanto48@yahoo.co.id>
2022-11-13 14:58:09 +0700

    aesx: example: add aes jpg file encrypt and decrypt

--- _examples/aes_file_decrypt/main.go
+++ _examples/aes_file_decrypt/main.go
@@ -12,26 +12,26 @@ func main() {
 	// digital signature with RSA
 	myFile, err := os.Open("decrypted_file.bin")
 	if err != nil {
-		fmt.Println("error: open my_file.txt")
+		fmt.Println("error: open decrypted_file")
 		os.Exit(1)
 	}
 
 	defer func() { myFile.Close() }()
 
-	key, _ := hex.DecodeString("6368616e676520746869732070617373")
+	key, _ := hex.DecodeString("6368616e6765207468697320706173736368616e676520746869732070617373")
 	decryptedData, err := io.ReadAll(myFile)
 	if err != nil {
-		fmt.Println("error: read all my_file.txt")
+		fmt.Println("error: read all decrypted_file")
 		os.Exit(1)
 	}
 
-	plainDataDecrypted, err := aesx.DecryptWithAES128GCM(key, decryptedData)
+	plainDataDecrypted, err := aesx.DecryptWithAES256GCM(key, decryptedData)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	outputFile, err := os.Create("out.txt")
+	outputFile, err := os.Create("out.jpg")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
--- _examples/aes_file_encrypt/main.go
+++ _examples/aes_file_encrypt/main.go
@@ -10,22 +10,22 @@ import (
 
 func main() {
 	// digital signature with RSA
-	myFile, err := os.Open("my_file.txt")
+	myFile, err := os.Open("Gopher.jpg")
 	if err != nil {
-		fmt.Println("error: open my_file.txt")
+		fmt.Println("error: open Gopher.jpg")
 		os.Exit(1)
 	}
 
 	defer func() { myFile.Close() }()
 
-	key, _ := hex.DecodeString("6368616e676520746869732070617373")
+	key, _ := hex.DecodeString("6368616e6765207468697320706173736368616e676520746869732070617373")
 	plainData, err := io.ReadAll(myFile)
 	if err != nil {
-		fmt.Println("error: read all my_file.txt")
+		fmt.Println("error: read all Gopher.jpg")
 		os.Exit(1)
 	}
 
-	aesData, err := aesx.EncryptWithAES128GCM(key, plainData)
+	aesData, err := aesx.EncryptWithAES256GCM(key, plainData)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
