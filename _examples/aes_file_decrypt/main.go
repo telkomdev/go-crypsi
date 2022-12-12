847564c29c65c32d2bc5c8f2c3dcf432d25a90b4
wuriyanto <wuriyanto48@yahoo.co.id>
2022-12-12 15:15:49 +0700

    aes: update example

--- _examples/aes_file_decrypt/main.go
+++ _examples/aes_file_decrypt/main.go
@@ -4,13 +4,12 @@ import (
 	"encoding/hex"
 	"fmt"
 	"github.com/telkomdev/go-crypsi/aesx"
-	"io"
 	"os"
 )
 
 func main() {
 	// digital signature with RSA
-	myFile, err := os.Open("decrypted_file.bin")
+	myFile, err := os.Open("../aes_file_encrypt/out.bin")
 	if err != nil {
 		fmt.Println("error: open decrypted_file")
 		os.Exit(1)
@@ -19,17 +18,6 @@ func main() {
 	defer func() { myFile.Close() }()
 
 	key, _ := hex.DecodeString("6368616e6765207468697320706173736368616e676520746869732070617373")
-	decryptedData, err := io.ReadAll(myFile)
-	if err != nil {
-		fmt.Println("error: read all decrypted_file")
-		os.Exit(1)
-	}
-
-	plainDataDecrypted, err := aesx.DecryptWithAES256GCM(key, decryptedData)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
 
 	outputFile, err := os.Create("out.jpg")
 	if err != nil {
@@ -39,7 +27,7 @@ func main() {
 
 	defer func() { outputFile.Close() }()
 
-	_, err = outputFile.Write(plainDataDecrypted)
+	err = aesx.DecryptWithAES256GCMIO(key, myFile, outputFile)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
--- _examples/aes_file_encrypt/main.go
+++ _examples/aes_file_encrypt/main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/hex"
 	"fmt"
 	"github.com/telkomdev/go-crypsi/aesx"
-	"io"
 	"os"
 )
 
@@ -19,17 +18,6 @@ func main() {
 	defer func() { myFile.Close() }()
 
 	key, _ := hex.DecodeString("6368616e6765207468697320706173736368616e676520746869732070617373")
-	plainData, err := io.ReadAll(myFile)
-	if err != nil {
-		fmt.Println("error: read all Gopher.jpg")
-		os.Exit(1)
-	}
-
-	aesData, err := aesx.EncryptWithAES256GCM(key, plainData)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
 
 	outputFile, err := os.Create("out.bin")
 	if err != nil {
@@ -39,7 +27,7 @@ func main() {
 
 	defer func() { outputFile.Close() }()
 
-	_, err = outputFile.Write(aesData)
+	err = aesx.EncryptWithAES256GCMIO(key, myFile, outputFile)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
